1dc8a8ba3df4087dc6cde333a20682eea84c4bd7
50vincents <vincethai035@gmail.com>
2020-09-17 11:45:16 -0700

    Updates

---
+++ precache-manifest.75487b8b84339773c471bfaa665813ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d763d68ca31d8d4d7811ee9f393f817d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "52a6dc2d6b7226d71e05",
+    "url": "/portfolio/static/css/main.0a506531.chunk.css"
+  },
+  {
+    "revision": "b7e837198d1d1eaf761d",
+    "url": "/portfolio/static/js/2.cbcd2f92.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.cbcd2f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a6dc2d6b7226d71e05",
+    "url": "/portfolio/static/js/main.ce61b649.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "eea742314c749181101c2f0332b4d661",
+    "url": "/portfolio/static/media/AMAZON.eea74231.png"
+  },
+  {
+    "revision": "9853d5100b66f4d00a491ea4fe551161",
+    "url": "/portfolio/static/media/IG.9853d510.png"
+  },
+  {
+    "revision": "f6c6cffccf0f402a38c8366ba35e93b3",
+    "url": "/portfolio/static/media/MOB.f6c6cffc.png"
+  },
+  {
+    "revision": "77bbffad510f983a0de33e1dba89e05e",
+    "url": "/portfolio/static/media/PROF.77bbffad.jpg"
+  },
+  {
+    "revision": "2c14393e7f1714cbc13cb486eb461422",
+    "url": "/portfolio/static/media/RESUMELATEST.2c14393e.pdf"
+  },
+  {
+    "revision": "1d2b6300e55104f1560ac7d4df28c5de",
+    "url": "/portfolio/static/media/SPOTIFY.1d2b6300.png"
+  }
+]);
